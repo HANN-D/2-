a3faf60ff6815a24cd1c057d89b614962311c705
taehan kim <ryan0207kr43@gmail.com>
2024-11-15 21:05:28 +0900

    테스트1

--- 2조/Team_project/Form1.Designer.cs
+++ 2조/Team_project/Form1.Designer.cs
@@ -42,10 +42,10 @@
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button1.Location = new System.Drawing.Point(80, 256);
+            this.button1.Location = new System.Drawing.Point(91, 320);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.Size = new System.Drawing.Size(101, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "GAME START";
             this.button1.UseVisualStyleBackColor = false;
@@ -55,9 +55,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button2.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.button2.Location = new System.Drawing.Point(80, 295);
+            this.button2.Location = new System.Drawing.Point(91, 369);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 18);
+            this.button2.Size = new System.Drawing.Size(101, 22);
             this.button2.TabIndex = 1;
             this.button2.Text = "EXIT";
             this.button2.UseVisualStyleBackColor = false;
@@ -68,11 +69,11 @@
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.textBox1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.textBox1.ForeColor = System.Drawing.Color.Black;
-            this.textBox1.Location = new System.Drawing.Point(10, 10);
+            this.textBox1.Location = new System.Drawing.Point(11, 12);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(237, 44);
+            this.textBox1.Size = new System.Drawing.Size(270, 53);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "총알 피하기";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -81,11 +82,11 @@
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.textBox2.Font = new System.Drawing.Font("휴먼둥근헤드라인", 7.8F, System.Drawing.FontStyle.Underline);
-            this.textBox2.Location = new System.Drawing.Point(10, 95);
+            this.textBox2.Location = new System.Drawing.Point(12, 11);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(238, 94);
+            this.textBox2.Size = new System.Drawing.Size(271, 116);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "룰 설명\r\n!캐릭터는 좌우로만 이동가능!\r\n\r\n!하늘에서 총알이 떨어진다!\r\n\r\n!총알에 맞으면 게임 끝!\r\n\r\n!EASY  NORMAL  HAR" +
     "D!\r\n";
@@ -93,12 +94,16 @@
             // 
             // EASY
             // 
+            this.EASY.AutoEllipsis = true;
             this.EASY.AutoSize = true;
             this.EASY.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.EASY.Cursor = System.Windows.Forms.Cursors.No;
+            this.EASY.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.EASY.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.EASY.Location = new System.Drawing.Point(10, 216);
+            this.EASY.Location = new System.Drawing.Point(11, 270);
+            this.EASY.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.EASY.Name = "EASY";
-            this.EASY.Size = new System.Drawing.Size(64, 16);
+            this.EASY.Size = new System.Drawing.Size(77, 20);
             this.EASY.TabIndex = 3;
             this.EASY.TabStop = true;
             this.EASY.Text = "EASY";
@@ -111,9 +116,10 @@
             this.NORMAL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.NORMAL.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.NORMAL.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.NORMAL.Location = new System.Drawing.Point(80, 216);
+            this.NORMAL.Location = new System.Drawing.Point(91, 270);
+            this.NORMAL.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.NORMAL.Name = "NORMAL";
-            this.NORMAL.Size = new System.Drawing.Size(89, 16);
+            this.NORMAL.Size = new System.Drawing.Size(108, 20);
             this.NORMAL.TabIndex = 4;
             this.NORMAL.TabStop = true;
             this.NORMAL.Text = "NORMAL";
@@ -125,9 +131,10 @@
             this.HARH.AutoSize = true;
             this.HARH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.HARH.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.HARH.Location = new System.Drawing.Point(175, 216);
+            this.HARH.Location = new System.Drawing.Point(200, 270);
+            this.HARH.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.HARH.Name = "HARH";
-            this.HARH.Size = new System.Drawing.Size(67, 16);
+            this.HARH.Size = new System.Drawing.Size(80, 20);
             this.HARH.TabIndex = 5;
             this.HARH.TabStop = true;
             this.HARH.Text = "HARD";
@@ -136,11 +143,11 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(258, 360);
+            this.ClientSize = new System.Drawing.Size(295, 450);
             this.Controls.Add(this.HARH);
             this.Controls.Add(this.NORMAL);
             this.Controls.Add(this.EASY);
@@ -148,6 +155,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
